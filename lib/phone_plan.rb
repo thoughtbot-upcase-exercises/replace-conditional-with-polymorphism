c4a33430a66915c10a394e78acc000e1433593e7
Melissa Xie <melissa@thoughtbot.com>
2014-12-15 12:51:03 -0500

    Use double quotes and break long lines

--- lib/phone_plan.rb
+++ lib/phone_plan.rb
@@ -6,14 +6,14 @@ class PhonePlan
   end
 
   def cost
-    if type == 'individual'
+    if type == "individual"
       number_of_phones * price
-    elsif type == 'family'
+    elsif type == "family"
       number_of_extra_phones = number_of_phones - 1
       cost_per_extra_phone = 10
 
       price + (number_of_extra_phones * cost_per_extra_phone)
-    elsif type == 'business'
+    elsif type == "business"
       subtotal = number_of_phones * price
 
       if number_of_phones < 50
--- spec/lib/phone_plan_spec.rb
+++ spec/lib/phone_plan_spec.rb
@@ -1,32 +1,48 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe PhonePlan do
-  describe '#cost' do
-    context 'when it is the individual plan' do
-      it 'multiples the price by the number of phones' do
-        phone_plan = PhonePlan.new(number_of_phones: 3, price: 40, type: 'individual')
+  describe "#cost" do
+    context "when it is the individual plan" do
+      it "multiples the price by the number of phones" do
+        phone_plan = PhonePlan.new(
+          number_of_phones: 3,
+          price: 40,
+          type: "individual"
+        )
         expect(phone_plan.cost).to eq 120
       end
     end
 
-    context 'when it is the family plan' do
-      it 'discounts each additional phone' do
-        phone_plan = PhonePlan.new(number_of_phones: 3, price: 40, type: 'family')
+    context "when it is the family plan" do
+      it "discounts each additional phone" do
+        phone_plan = PhonePlan.new(
+          number_of_phones: 3,
+          price: 40,
+          type: "family"
+        )
         expect(phone_plan.cost).to eq 60
       end
     end
 
-    context 'when it is the business plan' do
-      context 'when there are fewer than 50 phones' do
-        it 'returns the price with a 25% discount' do
-          phone_plan = PhonePlan.new(number_of_phones: 3, price: 40, type: 'business')
+    context "when it is the business plan" do
+      context "when there are fewer than 50 phones" do
+        it "returns the price with a 25% discount" do
+          phone_plan = PhonePlan.new(
+            number_of_phones: 3,
+            price: 40,
+            type: "business"
+          )
           expect(phone_plan.cost).to eq 90
         end
       end
 
-      context 'when there are 50 or more phones' do
-        it 'returns the price with a 50% discount' do
-          phone_plan = PhonePlan.new(number_of_phones: 51, price: 40, type: 'business')
+      context "when there are 50 or more phones" do
+        it "returns the price with a 50% discount" do
+          phone_plan = PhonePlan.new(
+            number_of_phones: 51,
+            price: 40,
+            type: "business"
+          )
           expect(phone_plan.cost).to eq 1020
         end
       end
